75554deb7ee94f057d19dbabff1c95c925dbe1ec
thegtzz <thegtzz@gmail.com>
2020-01-27 20:29:16 +0300

    Mouse cursor is hidden when game is started & and appears when it's over

--- game_functions.py
+++ game_functions.py
@@ -43,6 +43,8 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
     """Starts a new game after pushing Play-button."""
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
+        # Mouse cursor is hidden.
+        pygame.mouse.set_visible(False)
         # Reset game stats.
         stats.reset_stats()
         stats.game_active = True
@@ -166,6 +168,7 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
         sleep(0.5)
     else:
         stats.game_active = False
+        pygame.mouse.set_visible(True)
 
 
 def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
